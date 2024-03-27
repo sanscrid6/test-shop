9f2db869872fadb9544da194f4796f7279e13f2f
Ivan <sanscrid6@mail.ru>
2024-03-27 21:14:32 +0300

    up: money input

--- src/components/MoneyCounter/MoneyCounter.tsx
+++ src/components/MoneyCounter/MoneyCounter.tsx
@@ -1,9 +1,18 @@
 import { useUnit } from "effector-react";
 import styles from "./counter.module.css";
-import { $money, decreaseMoney, increaseMoney } from "../../entities/money";
+import {
+  $money,
+  decreaseMoney,
+  increaseMoney,
+  setMoney,
+} from "../../entities/money";
+import { useRef } from "react";
+import { clamp } from "../../utils/math-utils";
 
 function MoneyCounter() {
   const money = useUnit($money);
+  const ref = useRef<HTMLInputElement | null>(null);
+
   const delta = 10;
 
   function increaseHandler() {
@@ -14,19 +23,32 @@ function MoneyCounter() {
     decreaseMoney(delta);
   }
 
+  function enterMoneyHandler(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key.toLowerCase() === "enter") {
+      setMoney(clamp(+(ref.current?.value ?? 0), 0, 10000000));
+    }
+  }
+
   return (
     <div className={styles.container}>
-      <div
-        role="button"
-        onClick={decreaseHandler}
-        className={styles.minusButton}
-      >
-        -
+      <div className={styles.moneyContainer}>
+        <div
+          role="button"
+          onClick={decreaseHandler}
+          className={styles.minusButton}
+        >
+          -
+        </div>
+        <p>Money {money}</p>
+        <div
+          role="button"
+          onClick={increaseHandler}
+          className={styles.addButton}
+        >
+          +
+        </div>
       </div>
-      <p>{money}</p>
-      <div role="button" onClick={increaseHandler} className={styles.addButton}>
-        +
-      </div>
+      <input onKeyUp={enterMoneyHandler} type="number" ref={ref} />
     </div>
   );
 }
--- src/entities/money/index.ts
+++ src/entities/money/index.ts
@@ -3,7 +3,9 @@ import { createEvent, createStore } from "effector";
 export const $money = createStore<number>(100);
 export const increaseMoney = createEvent<number>();
 export const decreaseMoney = createEvent<number>();
+export const setMoney = createEvent<number>();
 
 $money
   .on(increaseMoney, (state, data) => state + data)
-  .on(decreaseMoney, (state, data) => (state - data < 0 ? 0 : state - data));
+  .on(decreaseMoney, (state, data) => (state - data < 0 ? 0 : state - data))
+  .on(setMoney, (_, data) => data);
